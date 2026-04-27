4d85d6f267f39e4570a034f6b516f40261fd83ed
Ibrahim Al-Bari <ibrahim2010963@gmail.com>
2026-04-27 20:44:45 +0300

    added nav + fixed fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import NavBar from './components/NavBar'
 
 const Page = () => {
     return (
+        <div>
         <main>
+            <NavBar />
 
         </main>
+        </div>
     )
 }
 export default Page
