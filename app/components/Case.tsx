b60d6b17846caaf78eb5b7c1b22626cfa1fb9e04
Ibrahim Al-Bari <ibrahim2010963@gmail.com>
2026-06-01 15:15:08 +0300

    deploy vercel

--- app/components/Case.tsx
+++ app/components/Case.tsx
@@ -8,7 +8,7 @@ import { useGLTF } from '@react-three/drei'
 import gsap from "gsap";
 import {useMediaQuery} from "react-responsive";
 
-export function Case(props) {
+export function Case(props: any) {
   const { nodes, materials } = useGLTF('/models/case.glb')
   const ref = useRef()
   const isMobile = useMediaQuery({ maxWidth: 768 });
